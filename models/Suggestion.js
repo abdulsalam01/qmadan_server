6d52de114477b667452e896e74af7cc737635565
abdulsalam <abdulsalam121196@gmail.com>
2020-06-10 01:21:37 +0700

    add model

---
+++ models/Suggestion.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const suggestionSchema = mongoose.Schema({
+  'body': {
+    type: String,
+    required: true
+  },
+  'by': String,
+  'phone': String,
+  'created_at': {
+    type: Date,
+    default: Date.now
+  }
+}, {collection: 'suggestion'});
+
+module.exports = mongoose.model('Suggestion', suggestionSchema);
