26007e8428aae815097f47b551f07402b15d6b4a
Abdul Salam <abdulsalam121196@gmail.com>
2021-02-15 14:42:57 +0700

    add netlify

--- app.js
+++ app.js
@@ -28,11 +28,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// api request
-app.use('/api', graphQLHttp({
+app.use('/.netlify/functions/api', graphQLHttp({
   schema: schemaApp,
   graphiql: true
-}))
+}));
+
+// api request
+// app.use('/api', graphQLHttp({
+//   schema: schemaApp,
+//   graphiql: true
+// }))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,11 @@ const mongoose = require('mongoose');
 const dbName = 'Qmadan_db';
 const isOffline = false;
 //
-const uri = isOffline ? `mongodb://localhost:27017/${dbName}` : 
-  `mongodb+srv://devops786:devops786@cluster0.4hdsq.mongodb.net/Qmadan_db?retryWrites=true&w=majority`;
-let db;
+const uri = isOffline ? 
+  `mongodb://localhost:27017/${dbName}` : 
+  `mongodb+srv://devops786:devops786@cluster0.4hdsq.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
+let db;
 // connect to mongodb
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).then(
   () => { console.log("Connected to ", db.name) },
