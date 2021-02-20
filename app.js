77a498365988cdb6bb6bdbd2302531e0586ce4a7
Abdul Salam <abdulsalam121196@gmail.com>
2021-02-20 14:51:35 +0700

    to header host

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static('uploads'));
 
 // base url getter
 app.use((req, res, next) => {
-  baseUrl = req.headers.origin;
+  baseUrl = true ? req.headers.host : req.headers.origin;
   next();
 });
 
